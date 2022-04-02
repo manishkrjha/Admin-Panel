4964c3e5ec2623991a65038f8ac94d0fb0fbc674
manishkrjha <manishkj2204@gmail.com>
2022-04-02 23:27:09 +0530

    Initial commit

---
+++ src/api/post.js
@@ -0,0 +1,18 @@
+import client from "./client";
+
+export const getPosts = async (pageNo, limit) => {
+    try{
+        const { data } = await client(`/post/posts?pageNo=${pageNo}&limit=${limit}`);
+        {console.log('Get post data: ')}
+        {console.log(data)}
+        return data;
+    } catch(error) {
+        const { response } = error;
+
+        if(response?.data){
+            return response.data;
+        }
+
+        return { error: error.message || error };  
+    }
+}
