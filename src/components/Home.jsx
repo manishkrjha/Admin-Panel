1e695950601adf9fc5ac2ceed317cca2405c6c90
manishkrjha <manishkj2204@gmail.com>
2022-04-04 23:53:55 +0530

    rendered posts in home

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,19 +1,25 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { getPosts } from '../api/post';
+import PostCard from './PostCard';
 
 let pageNo = 0;
 const POST_LIMIT = 9;
 
 export default function Home() {
 
+    const [posts, setPosts] = useState([]);
+    const [totalPostCount, setTotalPostCount] = useState([]);
+
 
     const fetchPosts = async () => {
-        const { error, posts } = await getPosts(pageNo, POST_LIMIT);
+        const { error, posts, postCount } = await getPosts(pageNo, POST_LIMIT);
 
         if(error) return console.log(error);
 
-        //ISSUE: Logs an empty array
+
         console.log(posts); 
+        setPosts(posts);
+        setTotalPostCount(postCount);
     };
 
     useEffect(() => {
@@ -22,7 +28,15 @@ export default function Home() {
 
     return (
         <div>
-            Home
+            <div className="grid grid-cols-3 gap-3 pb-5">
+            {
+                posts.map((post) => {
+                    return <PostCard key={post.id} post={post} />
+                }) 
+            }
+            </div>
+            
         </div>
+        // <div>Home | Working</div>
     )
 }
